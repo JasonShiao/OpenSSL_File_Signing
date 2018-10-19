247210a3af74e9ed26517dd08ac0c99f5c834246
Jau-Shiuan(Jason) Shiao <b01901105@ntu.edu.tw>
2018-10-19 16:07:27 +0800

    Generate CSR

--- main.c
+++ main.c
@@ -19,12 +19,13 @@
 
 #include <assert.h>
 
+int add_ext(X509 *cert, int nid, char *value);
 
 int main()
 {
 
-	// Parse existing certificate
-	char *path = "C:\\Users\\9518\\Documents\\Training_3\\OpenSSL_File_Signing\\OpenSSL_File_Signing\\openssl\\www.example.com.cert.pem";
+	// Parse an existing certificate
+	/*char *path = "C:\\Users\\9518\\Documents\\Training_3\\OpenSSL_File_Signing\\OpenSSL_File_Signing\\openssl\\www.example.com.cert.pem";
 	FILE *fp = NULL;
 	fopen_s(&fp, path, "r");
 	if (!fp) {
@@ -48,7 +49,279 @@ int main()
 	printf("%s\n", issuer);
 
 	X509_free(cert);
-	fclose(fp);
+	fclose(fp);*/
+
+
+	/**********************************************************************/
+	/*                       Create a CA certificate                      */
+	/**********************************************************************/
+
+	EVP_PKEY * CA_pkey;
+	CA_pkey = EVP_PKEY_new();
+
+	RSA * CA_rsa;
+	CA_rsa = RSA_generate_key(
+		4096,   /* number of bits for the key - 2048 is a sensible value */
+		RSA_F4, /* exponent - RSA_F4 is defined as 0x10001L */
+		NULL,   /* callback - can be NULL if we aren't displaying progress */
+		NULL    /* callback argument - not needed in this case */
+	);
+	EVP_PKEY_assign_RSA(CA_pkey, CA_rsa);
+
+
+	X509 * CA_cert;
+	CA_cert = X509_new();
+
+	ASN1_INTEGER_set(X509_get_serialNumber(CA_cert), 1);
+
+	/* Set serial number */
+	ASN1_INTEGER_set(X509_get_serialNumber(CA_cert), 1);
+	
+	/* Set start and expire date */
+	X509_gmtime_adj(X509_get_notBefore(CA_cert), 0);
+	X509_gmtime_adj(X509_get_notAfter(CA_cert), 31536000L); // in number of seconds
+	
+	/* Set public key */
+	X509_set_pubkey(CA_cert, CA_pkey);
+	
+	/* Set subject information */
+	X509_NAME * CA_name;
+	CA_name = X509_get_subject_name(CA_cert);
+	
+	/* Set country code(．C・), organization('O') and common name('CN') */
+	X509_NAME_add_entry_by_txt(CA_name, "C", MBSTRING_ASC,
+								(unsigned char *)"TW", -1, -1, 0);
+	X509_NAME_add_entry_by_txt(CA_name, "O", MBSTRING_ASC,
+								(unsigned char *)"UIC Inc.", -1, -1, 0);
+	X509_NAME_add_entry_by_txt(CA_name, "CN", MBSTRING_ASC,
+								(unsigned char *)"jasonshiao.com", -1, -1, 0);
+	/* Set issuer name:
+		self-signed: issuer name is equal to subject name */
+	X509_set_issuer_name(CA_cert, CA_name); // if it's not self-signed, the name should be acquired from issuer certificate
+
+	/* Add various extensions: standard extensions */
+	add_ext(CA_cert, CA_cert, NID_basic_constraints, "critical,CA:TRUE");
+	add_ext(CA_cert, CA_cert, NID_key_usage, "critical,keyCertSign,cRLSign");
+	add_ext(CA_cert, CA_cert, NID_subject_key_identifier, "hash");
+
+	/* CA self-signs the certificate */
+	if (!X509_sign(CA_cert, CA_pkey, EVP_sha256()))
+		return 1;
+
+
+	/* Output/Write the certificate into a .pem file or directly print with stderr */
+	FILE *CA_cert_fp;
+	fopen_s(&CA_cert_fp, "CA.crt.pem", "w");
+	PEM_write_X509(CA_cert_fp, CA_cert);
+	fclose(CA_cert_fp);
+
+
+	/* Output pkey file */
+	FILE *CA_pkey_fp;
+	fopen_s(&CA_pkey_fp, "CA.key", "w");
+
+	PEM_write_PrivateKey(
+		CA_pkey_fp,		/* write the key to the file we've opened */
+		CA_pkey,    /* key struct in the program */
+		NULL,		/* cipher for encrypting the key on disk */
+		NULL,		/* passphrase for the key encryption on disk */
+		-1,			/* length of the passphrase string */
+		NULL,		/* callback for requesting a password */
+		NULL		/* data to pass to the callback */
+	);
+
+	fclose(CA_pkey_fp);
+
+
+	/**********************************************************************/
+	/*                         Create a certificate                       */
+	/**********************************************************************/
+
+#ifdef SIGN_DIRECTLY
+	/* Get issuer name from CA cert */
+
+	
+
+	EVP_PKEY * endpoint_pkey;
+	endpoint_pkey = EVP_PKEY_new();
+
+	RSA * endpoint_rsa;
+	endpoint_rsa = RSA_generate_key(
+		4096,   /* number of bits for the key - 2048 is a sensible value */
+		RSA_F4, /* exponent - RSA_F4 is defined as 0x10001L */
+		NULL,   /* callback - can be NULL if we aren't displaying progress */
+		NULL    /* callback argument - not needed in this case */
+	);
+	EVP_PKEY_assign_RSA(endpoint_pkey, endpoint_rsa);
+
+
+	X509 * endpoint_cert;
+	endpoint_cert = X509_new();
+
+	ASN1_INTEGER_set(X509_get_serialNumber(endpoint_cert), 1);
+
+	/* Set serial number */
+	ASN1_INTEGER_set(X509_get_serialNumber(endpoint_cert), 1);
+
+	/* Set start and expire date */
+	X509_gmtime_adj(X509_get_notBefore(endpoint_cert), 0);
+	X509_gmtime_adj(X509_get_notAfter(endpoint_cert), 31536000L); // in number of seconds
+
+	/* Set public key */
+	X509_set_pubkey(endpoint_cert, endpoint_pkey);
+
+	/* Set subject information */
+	X509_NAME * endpoint_name;
+	endpoint_name = X509_get_subject_name(endpoint_cert);
+
+	/* Set country code(．C・), organization('O') and common name('CN') */
+	X509_NAME_add_entry_by_txt(endpoint_name, "C", MBSTRING_ASC,
+		(unsigned char *)"TW", -1, -1, 0);
+	X509_NAME_add_entry_by_txt(endpoint_name, "O", MBSTRING_ASC,
+		(unsigned char *)"UIC Inc.", -1, -1, 0);
+	X509_NAME_add_entry_by_txt(endpoint_name, "CN", MBSTRING_ASC,
+		(unsigned char *)"pc.jasonshiao.com", -1, -1, 0);
+	/* Set issuer name:
+	self-signed: issuer name is equal to subject name */
+	X509_set_issuer_name(endpoint_cert, CA_name); // if it's not self-signed, the name should be acquired from issuer certificate
+
+	/* Add various extensions: standard extensions */
+	add_ext(CA_cert, endpoint_cert, NID_basic_constraints, "critical,CA:FALSE");
+	add_ext(CA_cert, endpoint_cert, NID_subject_key_identifier, "hash");
+
+	/* CA signs the certificate for endpoint */
+	if (!X509_sign(endpoint_cert, CA_pkey, EVP_sha256()))
+		return 1;
+
+	/* Write the certificate into a .pem file or directly print with stderr */
+	FILE *endpoint_cert_fp;
+	fopen_s(&endpoint_cert_fp, "endpoint.crt.pem", "w");
+	PEM_write_X509(endpoint_cert_fp, endpoint_cert);
+	fclose(endpoint_cert_fp);
+
+
+	/* Output pkey file */
+	FILE *endpoint_pkey_fp;
+	fopen_s(&endpoint_pkey_fp, "endpoint.key", "w");
+
+	PEM_write_PrivateKey(
+		endpoint_pkey_fp,		/* write the key to the file we've opened */
+		endpoint_pkey,    /* key struct in the program */
+		NULL,		/* cipher for encrypting the key on disk */
+		NULL,		/* passphrase for the key encryption on disk */
+		-1,			/* length of the passphrase string */
+		NULL,		/* callback for requesting a password */
+		NULL		/* data to pass to the callback */
+	);
+
+	fclose(endpoint_pkey_fp);
+#else
+	/**********************************************************************/
+	/*                           Create a CSR                             */
+	/**********************************************************************/
+
+	EVP_PKEY * endpoint_pkey;
+	endpoint_pkey = EVP_PKEY_new();
+
+	RSA * endpoint_rsa;
+	endpoint_rsa = RSA_generate_key(
+		4096,   /* number of bits for the key - 2048 is a sensible value */
+		RSA_F4, /* exponent - RSA_F4 is defined as 0x10001L */
+		NULL,   /* callback - can be NULL if we aren't displaying progress */
+		NULL    /* callback argument - not needed in this case */
+	);
+	EVP_PKEY_assign_RSA(endpoint_pkey, endpoint_rsa);
+
+
+	// 2. set version of x509 req
+	int nVersion = 0; // must be 0?
+	X509_REQ *endpoint_cert_req = X509_REQ_new();
+	int ret = X509_REQ_set_version(endpoint_cert_req, nVersion);
+	if (ret != 1) {
+		X509_REQ_free(endpoint_cert_req);
+		EVP_PKEY_free(endpoint_pkey);
+	}
+
+	// 3. set subject of x509 req
+	X509_NAME * endpoint_name;
+	endpoint_name = X509_REQ_get_subject_name(endpoint_cert_req);
+
+	ret = X509_NAME_add_entry_by_txt(endpoint_name, "C", MBSTRING_ASC, "TW", -1, -1, 0);
+	if (ret != 1) {
+		X509_REQ_free(endpoint_cert_req);
+		EVP_PKEY_free(endpoint_pkey);
+	}
+
+	ret = X509_NAME_add_entry_by_txt(endpoint_name, "ST", MBSTRING_ASC, "Taiwan", -1, -1, 0);
+	if (ret != 1) {
+		X509_REQ_free(endpoint_cert_req);
+		EVP_PKEY_free(endpoint_pkey);
+	}
+
+	ret = X509_NAME_add_entry_by_txt(endpoint_name, "L", MBSTRING_ASC, "Taipei", -1, -1, 0);
+	if (ret != 1) {
+		X509_REQ_free(endpoint_cert_req);
+		EVP_PKEY_free(endpoint_pkey);
+	}
+
+	ret = X509_NAME_add_entry_by_txt(endpoint_name, "O", MBSTRING_ASC, "UIC", -1, -1, 0);
+	if (ret != 1) {
+		X509_REQ_free(endpoint_cert_req);
+		EVP_PKEY_free(endpoint_pkey);
+	}
+
+	ret = X509_NAME_add_entry_by_txt(endpoint_name, "CN", MBSTRING_ASC, "pc.jasonshiao.com", -1, -1, 0);
+	if (ret != 1) {
+		X509_REQ_free(endpoint_cert_req);
+		EVP_PKEY_free(endpoint_pkey);
+	}
+
+
+	// 4. set public key of x509 req
+	ret = X509_REQ_set_pubkey(endpoint_cert_req, endpoint_pkey);
+	if (ret != 1) {
+		X509_REQ_free(endpoint_cert_req);
+		EVP_PKEY_free(endpoint_pkey);
+	}
+
+	// 5. Endpoint signs the CSR 
+	ret = X509_REQ_sign(endpoint_cert_req, endpoint_pkey, EVP_sha256());    // return x509_req->signature->length
+	if (ret <= 0) {
+		X509_REQ_free(endpoint_cert_req);
+		EVP_PKEY_free(endpoint_pkey);
+	}
+
+	/* Write the CSR into a .pem file or directly print with stderr */
+	FILE *endpoint_csr_fp;
+	fopen_s(&endpoint_csr_fp, "endpoint.csr.pem", "w");
+	PEM_write_X509_REQ(endpoint_csr_fp, endpoint_cert_req);
+	fclose(endpoint_csr_fp);
+
+
+	/* Output pkey file */
+	FILE *endpoint_pkey_fp;
+	fopen_s(&endpoint_pkey_fp, "endpoint.key", "w");
+
+	PEM_write_PrivateKey(
+		endpoint_pkey_fp,		/* write the key to the file we've opened */
+		endpoint_pkey,    /* key struct in the program */
+		NULL,		/* cipher for encrypting the key on disk */
+		NULL,		/* passphrase for the key encryption on disk */
+		-1,			/* length of the passphrase string */
+		NULL,		/* callback for requesting a password */
+		NULL		/* data to pass to the callback */
+	);
+
+	fclose(endpoint_pkey_fp);
+
+	/**********************************************************************/
+	/*               Sign a certificate based on the CSR                  */
+	/**********************************************************************/
+	
+
+
+
+#endif
 
 
 	/*********************************************************************/
@@ -70,7 +343,7 @@ int main()
 
 	/* Sign the text */
 	EVP_MD_CTX *mdctx = NULL;
-	int ret = 0;
+	ret = 0;
 	char *msg = "Test Message";
 
 	unsigned char **sig = malloc(sizeof(unsigned char*));
@@ -104,6 +377,10 @@ int main()
 
 	fclose(fp2);
 
+
+
+
+
 	/*********************************************************************/
 	/*                       Signature Verification                      */
 	/*********************************************************************/
@@ -111,7 +388,6 @@ int main()
 	/* Initialize `key` with a public key */
 	if (1 != EVP_DigestVerifyInit(mdctx, NULL, EVP_sha256(), NULL, pkey)) goto err;
 
-	/* Initialize `key` with a public key */
 	if (1 != EVP_DigestVerifyUpdate(mdctx, msg, strlen(msg))) goto err;
 
 	if (1 == EVP_DigestVerifyFinal(mdctx, *sig, slen))
@@ -142,4 +418,28 @@ err:
 	system("pause");
 	
 	return 0;
+}
+
+
+
+int add_ext(X509 *CA_cert, X509 *new_cert, int nid, char *value)
+{
+	X509_EXTENSION *ex;
+	X509V3_CTX ctx;
+
+	/* This sets the 'context' of the extensions. */
+	/* No configuration database */
+	X509V3_set_ctx_nodb(&ctx);
+
+	/* Issuer and subject certs: both the target since it is self signed,
+	* no request and no CRL
+	*/
+	X509V3_set_ctx(&ctx, CA_cert, new_cert, NULL, NULL, 0);
+	ex = X509V3_EXT_conf_nid(NULL, &ctx, nid, value);
+	if (!ex)
+		return 0;
+
+	X509_add_ext(new_cert, ex, -1);
+	X509_EXTENSION_free(ex);
+	return 1;
 }
